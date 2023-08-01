9d33ae025f6cf7cac352dfd2074c49aa21f72afd
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-08-01 09:21:49 -0700

    Contact Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    alert(`Thank you for submitting:
+    ${name}
+    ${email}
+    ${phone}`);
+  };
+
   return (
     <>
       <h1>Contact</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-        nesciunt repudiandae nisi deserunt quas ut eligendi voluptatibus odio
-        iure quidem. Suscipit soluta, nostrum voluptas porro minus officiis
-        amet! Ipsam, explicabo?
-      </p>
+
+      <form onSubmit={submitHandler}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Phone Number:
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
     </>
   );
 };
